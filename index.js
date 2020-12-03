a14fc6f01ebfaaf2f906f095b1d2b41e2e722e3d
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-03 22:12:19 +0300

    feat: create MainRouter

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (request, response) => {
-  // response.sendFile(path.join(__dirname, "about.html"));
-  response.render("main");
+app.get("/", (req, res) => {
+  res.render("main");
 });
+
+app.get("/user", () => {
+  res.send("User Page");
+});
+
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
 });
