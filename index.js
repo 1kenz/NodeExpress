ed189b7a9007b0147e57312518b4787cc94b4bfe
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-03 21:49:46 +0300

    feat: add and install ejs view

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const path = require("path");
 const port = process.env.port || 5000;
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "about.html"));
+  // response.sendFile(path.join(__dirname, "about.html"));
+  response.render("main");
 });
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
 });
-
