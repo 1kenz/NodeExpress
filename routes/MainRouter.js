3e3f74628a3c4a376a52f5031d5a334ddd7df057
xkendx <63884610+xkendx@users.noreply.github.com>
2020-12-03 23:12:43 +0300

    feat: convert dynamic pages

--- routes/MainRouter.js
+++ routes/MainRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
-  res.render("main");
+  res.render("main", { name: req.query.name });
 });
 
 router.post("/", (req, res) => {
